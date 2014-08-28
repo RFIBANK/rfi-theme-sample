f63cb08a1bfe76636da4e1f4d91357d557862e1a
Alexey Lebedeff <binarin@gmail.com>
2014-08-28 10:14:03 +0400

    Proper directory structure

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+  <head>
+    <title>Образец темы для пластиковых карт</title>
+  </head>
+  <body>
+    <a href="https://github.com/binarin/rfi-theme-sample">Пример и документация</a>
+  </body>
+</html>
